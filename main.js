0c1853c0253a176bb21d8a05f7fb66070c6c8560
karamveer <karamveer.garathi.9@gmail.com>
2022-08-27 00:09:15 +0530

    task10

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-console.log(a);
-console.log('p');
+// Put DOM elements into variables
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  var n = nameInput.value;
+  var e =emailInput.value;
+  localStorage.setItem('Name',n);
+  localStorage.setItem('Email',e);
+}
+
