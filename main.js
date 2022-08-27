5ea62391802debb7f3bb57c7d5fd31341f7a9d71
karamveer <karamveer.garathi.9@gmail.com>
2022-08-27 08:17:20 +0530

    task11

--- main.js
+++ main.js
@@ -5,15 +5,36 @@ const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
+var read_all_data=document.getElementById("read_all_data")
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
 
+
+//adding element in object
 function onSubmit(e) {
   e.preventDefault();
   
-  var n = nameInput.value;
-  var e =emailInput.value;
-  localStorage.setItem('Name',n);
-  localStorage.setItem('Email',e);
+  let myObj = {
+      name : nameInput.value,
+      email : emailInput.value
+  };
+  
+  myObj_serialized=JSON.stringify(myObj);
+  localStorage.setItem('myObj',myObj_serialized);
+
+  myObj.deserialized=JSON.parse(localStorage.getItem('myObj'));
+  console.log(myObj.deserialized);
+  
+  
 }
 
+
+
+read_all_data.onclick=function(e){
+  var keys=Object.keys(localStorage);
+  console.log(keys);
+
+  for(var key of keys){
+      console.log("Key : "+key+" : Value : "+localStorage.getItem(key));
+  }
+}
